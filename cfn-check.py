89df0f94a5fe76a86233c78116fa01f0132f5974
root <vitorrio.brooks@rightbrainnetworks.com>
2016-02-18 23:52:58 -0500

    basic parsing we're doing

--- cfn-check.py
+++ cfn-check.py
@@ -1,3 +1,14 @@
 import json
 import requests
 import boto3
+
+template_file = open('top.json')
+template = json.load(template_file)
+template_file.close()
+
+aws_type = ''AWS::CloudFormation::Stack
+
+for resource in template['Resources']:
+    if template['Resources'][resource]['Type'] == aws_type:
+        template['Resources'][resource]['Properties']['Parameters']['ASGHealthCheckType']
+
