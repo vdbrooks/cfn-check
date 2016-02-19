5251c1bf03f61863cc22779315070435900af9ec
root <vitorrio.brooks@rightbrainnetworks.com>
2016-02-19 00:03:47 -0500

    adding example template, few other updates

--- cfn-check.py
+++ cfn-check.py
@@ -9,6 +9,11 @@ template_file.close()
 aws_type = ''AWS::CloudFormation::Stack
 
 for resource in template['Resources']:
-    if template['Resources'][resource]['Type'] == aws_type:
-        template['Resources'][resource]['Properties']['Parameters']['ASGHealthCheckType']
+    health_type = ''
+    try:
+        if template['Resources'][resource]['Type'] == aws_type:
+            health_type = template['Resources'][resource]['Properties']['Parameters']['ASGHealthCheckType']
+    except KeyError, ke:
+        print "ran into an error: " + ke
+        sys.exit(1)
 
