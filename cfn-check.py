0bf78d49a623faeca23335d398969fc9d7b8d2d1
root <vitorrio.brooks@rightbrainnetworks.com>
2016-02-19 13:24:48 -0500

    finished all the basic logic

--- cfn-check.py
+++ cfn-check.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import json, sys
+import yaml
 import requests, pprint
 import boto3
 
@@ -8,13 +9,24 @@ template_file = open('top.json')
 template = json.load(template_file)
 template_file.close()
 
+#config_file = open(config/config.json)
+#config = json.load(config_file)
+#config_file.json()
+
+config_file = open('config/config.yml')
+config = yaml.load(config_file)
+
 aws_type = 'AWS::CloudFormation::Stack'
 
 for resource in template['Resources']:
     health_type = ''
     try:
-        print ""
         if template['Resources'][resource]['Type'] == aws_type:
+            for entity in config['tests']['scaling-policies'][0]['ignored-stacks']:
+                if resource == entity:
+                    print resource
+                    print "This is an ignored stack"
+                    pass
             health_type = template['Resources'][resource]['Properties']['Parameters']['ASGHealthCheckType']
             print "RESOURCE: " + str(resource) 
             print "HEALTH CHECK TYPE: " + str(health_type)
