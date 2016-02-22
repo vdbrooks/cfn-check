7ad901d720ee5117d8bf8318cd8f34fef36f95a4
Vitorrio Brooks <vitorrio.brooks@gmail.com>
2016-02-22 17:24:52 -0500

    Update cfn-check.py

--- cfn-check.py
+++ cfn-check.py
@@ -19,22 +19,15 @@ config = yaml.load(config_file)
 aws_type = 'AWS::CloudFormation::Stack'
 
 for resource in template['Resources']:
-    health_type = ''
-    try:
-        if template['Resources'][resource]['Type'] == aws_type:
-            for entity in config['tests']['scaling-policies'][0]['ignored-stacks']:
-                if resource == entity:
-                    print resource
-                    print "This is an ignored stack"
-                    pass
-            health_type = template['Resources'][resource]['Properties']['Parameters']['ASGHealthCheckType']
-            print "RESOURCE: " + str(resource) 
-            print "HEALTH CHECK TYPE: " + str(health_type)
-            print ""
+    if template['Resources'][resource]['Type'] == aws_type:
+        if entity in config['tests']['scaling-policies'][0]['ignored-stacks']:
+                print resource
+                print "This is an ignored stack"
+        health_type = template['Resources'][resource]['Properties']['Parameters'].get('ASGHealthCheckType', None)
+            if health_type:
+                print "RESOURCE: " + str(resource) 
+                print "HEALTH CHECK TYPE: " + str(health_type)
+                print ""
             
-    except KeyError, ke:
-        print "RESOURCE: " + str(resource) 
-        print "ERROR: " + "ASGHealthCheckType is not defined in this stack"
-        print ""
-        pass
+
 
